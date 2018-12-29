62118dedcd7866d1e1177190a38deb1a8070d509
Marcin Łesek <daav95@gmail.com>
2018-12-29 23:16:33 +0100

    Update gitignore and remove dist files

--- packages/core/dist/clone.js
+++
@@ -1,5 +0,0 @@
-// TODO create better solution for cloning initial values in form component
-export const clone = (value = null) => {
-    const json = JSON.stringify(value);
-    return JSON.parse(json);
-};
